bced48538ded8add6b0a67646736269efca7af8d
Kevin Lanni <therealklanni@gmail.com>
2016-10-22 15:45:10 -0700

    Update language migration and add seeds

--- priv/repo/migrations/3_create_language.exs
+++ priv/repo/migrations/3_create_language.exs
@@ -3,23 +3,8 @@ defmodule Org.Repo.Migrations.CreateLanguage do
 
   def change do
     create table(:languages) do
-      add :es5, :boolean, default: false, null: false
-      add :es6, :boolean, default: false, null: false
-      add :es7, :boolean, default: false, null: false
-      add :rails, :boolean, default: false, null: false
-      add :dotnet, :boolean, default: false, null: false
-      add :python, :boolean, default: false, null: false
-      add :php, :boolean, default: false, null: false
-      add :elixir, :boolean, default: false, null: false
-      add :elm, :boolean, default: false, null: false
-      add :typescript, :boolean, default: false, null: false
-      add :purescript, :boolean, default: false, null: false
-      add :go, :boolean, default: false, null: false
-      add :user_id, references(:users, on_delete: :nothing)
-
-      timestamps()
+      add :name, :string, null: false
+      add :title, :string, null: false
     end
-    create index(:languages, [:user_id])
-
   end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,5 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Org.Repo
+alias Org.Language
 
-Org.Repo.insert!(%Org.Language{})
+Repo.insert! %Language{name: "es5", title: "ES5"}
+Repo.insert! %Language{name: "es6", title: "ES6/ES2015"}
+Repo.insert! %Language{name: "es7", title: "ES7/ES2016"}
+Repo.insert! %Language{name: "typescript", title: "TypeScript"}
+Repo.insert! %Language{name: "purescript", title: "PureScript"}
+Repo.insert! %Language{name: "dotnet", title: ".NET"}
+Repo.insert! %Language{name: "elixir", title: "Elixir"}
+Repo.insert! %Language{name: "elm", title: "Elm"}
+Repo.insert! %Language{name: "rails", title: "Ruby on Rails"}
+Repo.insert! %Language{name: "python", title: "Python"}
+Repo.insert! %Language{name: "php", title: "PHP"}
+Repo.insert! %Language{name: "go", title: "Go"}
