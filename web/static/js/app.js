6d07e8cf5ad05c8fcb93fe9f6fc6f5a96e7ecf4b
Troy Mullaney <troym9731@gmail.com>
2017-01-09 12:33:20 -0700

    Add generic card and limit the card description with JS

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,1 +1,10 @@
 import "phoenix_html"
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.card__description').forEach(card => {
+    const charLimit = 150
+    if (card.innerText.length > charLimit) {
+      card.innerText = `${card.innerText.substring(0, charLimit)}...`
+    }
+  })
+})
