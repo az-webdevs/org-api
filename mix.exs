1248878b4f6707dd6ea6f6c6182c8989d12e94f3
Troy Mullaney <troym9731@gmail.com>
2017-04-17 22:04:12 -0700

    Align keyword list properly

--- mix.exs
+++ mix.exs
@@ -50,6 +50,6 @@ defmodule Org.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-    "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
   end
 end
